e69cf322b60efe072f10f16204684b092bc4c32e
Nancy Bankson <nbankson3@gmail.com>
2026-01-16 15:09:29 -0600

    Server connects correctly

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const Book = require("./models/Book");
-const connectDB = require("./db/connection");
-const mongoose = require('mongoose');
+// const express = require('express');
+// const app = express();
+// const Book = require("./models/Book");
+// const connectDB = require("./db/connection");
+// const mongoose = require('mongoose');
 require("dotenv").config();
-const PORT = process.env.PORT || 3001;
-const uri = process.env.MONGO_URI;
+// const PORT = process.env.PORT || 3001;
+// const uri = process.env.MONGO_URI;
 
 // try {
 //     await connectDB();
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const app = express();
-const Book = require("./models/Book");
+// const Book = require("./models/Book");
 const connectDB = require("./db/connection");
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
-const uri = process.env.MONGO_URI;
+// const uri = process.env.MONGO_URI;
+const bookRouter = require("./routes/bookRoutes");
 
-try {
-    await connectDB();
-    app.listen(PORT, () => {
+connectDB();
+
+// try {
+//     await connectDB();
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error('Failed to start server:', error);
+//   }
+
+  app.use(express.json());
+  app.use(express.urlencoded());
+
+  app.use('/', bookRouter);
+
+
+  app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  } catch (error) {
-    console.error('Failed to start server:', error);
-  }
