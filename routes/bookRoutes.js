f2b9c6421ef887d9964e4d172098d7ab84d6db5c
Nancy Bankson <nbankson3@gmail.com>
2026-01-16 13:50:50 -0600

    Added routes

---
+++ routes/bookRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const Book = require("./models/Book");
+const connectDB = require("./db/connection");
+const mongoose = require('mongoose');
+require("dotenv").config();
+const PORT = process.env.PORT || 3001;
+const uri = process.env.MONGO_URI;
+
+// try {
+//     await connectDB();
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error('Failed to start server:', error);
+//   }
+
+  const express = require('express');
+const router = express.Router();
+const bookController = require('../controllers/bookController');
+
+router.get('/', bookController.getAllBooks);
+router.get('/:id', bookController.getSingleBook);
+router.post('/', bookController.createNewBook);
+router.put('/:id', bookController.updateBook);
+router.delete('/:id', bookController.deleteBook);
+
+module.exports = router;
